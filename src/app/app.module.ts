9562cdbb34d58050b1afdc8c77de976b923eba67
magnummm <mail@sitepara.de>
2022-12-08 11:01:00 +0100

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule} from '@angular/fire/compat';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { environment } from 'environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'lernkarten-angular-firebase',
+    appId: '1:1833224008:web:b5e2520e231a53a2c4557d',
+    databaseURL: 'https://lernkarten-angular-firebase-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'lernkarten-angular-firebase.appspot.com',
+    apiKey: 'AIzaSyDP_dhgXJtgJtdhf_bEn35GJvg3zSUiXgk',
+    authDomain: 'lernkarten-angular-firebase.firebaseapp.com',
+    messagingSenderId: '1833224008',
+  },
+  production: true
+};
